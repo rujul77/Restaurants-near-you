8844c921ae98221f88405757f429ee56f0e96348
rujul77 <rujultiwari10@hotmail.com>
2026-03-27 22:50:54 +0000

    separated logic into two functions for easier readability and maintainability

--- backend.py
+++ backend.py
@@ -1,8 +1,34 @@
 from flask import Flask, request, render_template #this request gets data from the input - sent as post
 import requests #sends request to our outlined api link
-import json
+
+import pytest #unit testing
 
 app = Flask(__name__)
+
+def restaurant_list(postcode): 
+    api_link =  f"https://uk.api.just-eat.io/discovery/uk/restaurants/enriched/bypostcode/{postcode}"
+    headers = {"User-Agent": "Mozilla/5.0"}
+
+    get_api = requests.get(api_link, headers=headers, timeout=10) #dont wait forever for server
+    #if unable to get api data - cause an error and stop the program
+    get_api.raise_for_status()
+    final_data = get_api.json() #raw data into json
+
+    restaurants = []
+    for restaurant in final_data.get("restaurants")[:10]: #only the first 10 restaurant keys
+        cuisines = ""
+        for cuisine in restaurant["cuisines"]:
+            cuisines+= ", ".join(cuisine.get("name"))
+        
+        restaurants.append({
+            "name": restaurant["name"],
+            "cuisines": cuisines,
+            "rating": f'{restaurant["rating"]["starRating"]}({restaurant["rating"]["count"]})',
+            "address": f'{restaurant["address"]["firstLine"], {restaurant["address"]["postalCode"]}}',
+            "logo": restaurant["logoUrl"]
+        })
+    
+    return restaurants
 
 @app.route("/", methods=["GET", "POST"])
 def home():
@@ -10,45 +36,20 @@ def home():
     error = None
     if request.method == "POST":
         postcode = request.form.get("postcode").strip().replace(" ", "") #if something like abcd 123 it becomes abcd123
-
+        
+        #if user tries to enter without typing anything
         if not postcode:
             error = "Please enter a postcode!"
-        
         else:
-            #our api link
-            api_link = f"https://uk.api.just-eat.io/discovery/uk/restaurants/enriched/bypostcode/{postcode}"
-            #without this - 403 cloudflare error
-            headers = {"User-Agent": "Mozilla/5.0"}
-
             try:
-                #raw data from api stored here
-                get_api = requests.get(api_link, headers=headers)
-                finalData = get_api.json() #turn into json format
-                unfiltered_restaurant = finalData.get("restaurants", [])[:10]
-                
-                #if there are no restaurants found in the list[]
-                if not unfiltered_restaurant:
-                    error = "No restaurants found for that postcode!"
-                else:
+                #calling the function above to fill our array
+                restaurants = restaurant_list(postcode)
+                if not restaurants:
+                    error= "No restaurants found for this postcode"
 
-                    #loop through the list, and append what i need to restaurants[]
-                    for restaurant in unfiltered_restaurant:
-                        cuisines = ", ".join(cuisine["name"] for cuisine in restaurant["cuisines"])
-                        restaurants.append({
-                            "name":restaurant["name"],
-                            
-                            "cuisines": cuisines,
-
-                            "address": f'{restaurant["address"]["firstLine"]}, {restaurant["address"]["postalCode"]}',
-
-                            "rating": f'{restaurant["rating"]["starRating"]}({restaurant["rating"]["count"]})',
-
-                            "logo": restaurant["logoUrl"]
-                        })
-            
             #anything other than above goes wrong    
-            except:
-                error = "Something went wrong!"
+            except requests.exceptions.RequestException:
+                error="Something went wrong"                
         
     return render_template("index.html", restaurants=restaurants, error=error)
     
